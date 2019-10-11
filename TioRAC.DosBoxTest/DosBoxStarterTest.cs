202d01c4aec6a4f0ca0599dd8392cfc31601dfff
TioRAC's <ricardo@macwingo.com.br>
2019-10-11 17:20:46 -0300

    DosBox starter

---
+++ TioRAC.DosBoxTest/DosBoxStarterTest.cs
@@ -0,0 +1,19 @@
+﻿using TioRAC.DosBox;
+using NUnit.Framework;
+
+
+namespace TioRAC.DosBoxTest
+{
+    public class DosBoxStarterTest
+    {
+        [Test]
+        public void AAA()
+        {
+            var dosboxProcess = DosBoxStarter.FromProcess("dosbox.exe")
+                .SetParameters(a => a.AddExit().AddFullscreen())
+                .Start();
+
+
+        }
+    }
+}
