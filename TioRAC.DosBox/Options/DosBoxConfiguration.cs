4ea71a0473f4dd7a2c05ca581f326e01e71ba87e
Ricardo Coelho <tiorac@msn.com>
2019-10-12 14:18:14 -0300

    SdlOptions XML document and Fluent methods

---
+++ TioRAC.DosBox/Options/DosBoxConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TioRAC.DosBox.Options
+{
+    public class DosBoxConfiguration
+    {
+        public SdlOptions SDL { get; set; }
+    }
+}
--- TioRAC.DosBox/Options/SdlOptions.cs
+++ TioRAC.DosBox/Options/SdlOptions.cs
@@ -5,15 +5,25 @@ using System.Text;
 namespace TioRAC.DosBox.Options
 {
     /// <summary>
-    /// https://en.wikipedia.org/wiki/Computer_display_standard
+    /// <para>SDL Options for DosBox</para>
+    /// <para>See more in <a href="https://www.dosbox.com/wiki/Configuration:SDL">DosBox Configuriotn SDL</a> Wiki page</para>
     /// </summary>
     public class SdlOptions : BaseOptions
     {
+        #region "Constructions"
+
+        /// <summary>
+        /// Create instance of SDL options class
+        /// </summary>
         public SdlOptions()
             : base("sdl")
         {
 
         }
+
+        #endregion "Constructions"
+
+        #region "Fields"
 
         private bool? _fullscreen;
         private bool? _fulldouble;
@@ -28,7 +38,14 @@ namespace TioRAC.DosBox.Options
         private string _mapperfile;
         private bool? _useScanCodes;
 
+        #endregion "Fields"
 
+        #region "Properties"
+
+        /// <summary>
+        /// Start DOSBox directly in fullscreen.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public bool? FullScreen
         {
             get
@@ -42,6 +59,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Use double buffering in fullscreen.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public bool? FullDouble
         {
             get
@@ -55,6 +76,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Scale the application to this size IF the output device supports hardware scaling 
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public Resolution? FullResolution
         {
             get
@@ -68,6 +93,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Scale the window to this size IF the output device supports hardware scaling
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public Resolution? WindowResolution
         {
             get
@@ -81,6 +110,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// What to use for output. Surface does not support scaling or aspect correction.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public VideoOutput? Output
         {
             get
@@ -94,6 +127,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Mouse will automatically lock, if you click on the screen.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public bool? AutoLock
         {
             get
@@ -107,6 +144,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Mouse sensitivity. 1..1000
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public int? Sensitivity
         {
             get
@@ -115,11 +156,21 @@ namespace TioRAC.DosBox.Options
             }
             set
             {
-                _sensitivity = value;
+                if (value < 1)
+                    _sensitivity = 1;
+                else if (value > 1000)
+                    _sensitivity = 1000;
+                else
+                    _sensitivity = value;
+
                 OnPropertyChanged("Sensitivity");
             }
         }
 
+        /// <summary>
+        /// Wait before closing the console if DOSBox has an error.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public bool? WaitOnError
         {
             get
@@ -133,6 +184,11 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// <para>Priority levels for DOSBox.</para>
+        /// <para>Priority for when DosBox is focused</para>
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public PriorityLevel? PriorityFocused
         {
             get
@@ -146,6 +202,11 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// <para>Priority levels for DOSBox.</para>
+        /// <para>Priority for when DosBox is minimized</para>
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public PriorityLevel? PriorityMinimized
         {
             get
@@ -159,6 +220,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// File used to load/save the key/event mappings from.
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public string MapperFile
         {
             get
@@ -172,6 +237,10 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        /// <summary>
+        /// Avoid usage of symkeys, might not work on all operating systems
+        /// </summary>
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
         public bool? UseScanCodes
         {
             get
@@ -185,5 +254,153 @@ namespace TioRAC.DosBox.Options
             }
         }
 
+        #endregion "Properties"
+
+        #region "Fluent"
+
+        /// <summary>
+        /// Create a new SDL Options for DosBox configuration
+        /// </summary>
+        /// <returns></returns>
+        public static SdlOptions Create()
+        {
+            return new SdlOptions();
+        }
+
+        /// <summary>
+        /// Start DOSBox directly in fullscreen.
+        /// </summary>
+        /// <param name="fullscreen">Start DOSBox directly in fullscreen.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddFullScreen(bool? fullscreen = true)
+        {
+            FullScreen = fullscreen;
+            return this;
+        }
+
+        /// <summary>
+        /// Use double buffering in fullscreen.
+        /// </summary>
+        /// <param name="fulldouble">Use double buffering in fullscreen.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddFullDouble(bool? fulldouble = true)
+        {
+            FullDouble = fulldouble;
+            return this;
+        }
+
+        /// <summary>
+        /// Scale the application to this size IF the output device supports hardware scaling 
+        /// </summary>
+        /// <param name="resolution">Resolution the application</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddFullResolution(Resolution? resolution)
+        {
+            FullResolution = resolution;
+            return this;
+        }
+
+        /// <summary>
+        /// Scale the window to this size IF the output device supports hardware scaling
+        /// </summary>
+        /// <param name="resolution">Windows resolution</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddWindowResolution(Resolution? resolution)
+        {
+            WindowResolution = resolution;
+            return this;
+        }
+
+        /// <summary>
+        /// What to use for output. Surface does not support scaling or aspect correction.
+        /// </summary>
+        /// <param name="output">What to use for output. Surface does not support scaling or aspect correction.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddOutput(VideoOutput? output = VideoOutput.Surface)
+        {
+            Output = output;
+            return this;
+        }
+
+        /// <summary>
+        /// Mouse will automatically lock, if you click on the screen.
+        /// </summary>
+        /// <param name="autoLock">Mouse will automatically lock, if you click on the screen.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddAutoLock(bool? autoLock = true)
+        {
+            AutoLock = autoLock;
+            return this;
+        }
+
+        /// <summary>
+        /// Mouse sensitivity. 1..1000
+        /// </summary>
+        /// <param name="sensitivity">Mouse sensitivity. 1..1000</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddSensitivity(int? sensitivity = 100)
+        {
+            Sensitivity = sensitivity;
+            return this;
+        }
+
+        /// <summary>
+        /// Wait before closing the console if DOSBox has an error.
+        /// </summary>
+        /// <param name="waitOnError">Wait before closing the console if DOSBox has an error.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddWaitOnError(bool? waitOnError = true)
+        {
+            WaitOnError = waitOnError;
+            return this;
+        }
+
+        /// <summary>
+        /// <para>Priority levels for DOSBox.</para>
+        /// <para>Priority for when DosBox is focused</para>
+        /// </summary>
+        /// <param name="priorityFocused">Priority levels for DOSBox</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddPriorityFocused(PriorityLevel? priorityFocused = PriorityLevel.Higher)
+        {
+            PriorityFocused = priorityFocused;
+            return this;
+        }
+
+        /// <summary>
+        /// <para>Priority levels for DOSBox.</para>
+        /// <para>Priority for when DosBox is minimized</para>
+        /// </summary>
+        /// <param name="priorityMinimized">Priority levels for DOSBox</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddPriorityMinimized(PriorityLevel? priorityMinimized = PriorityLevel.Normal)
+        {
+            PriorityMinimized = priorityMinimized;
+            return this;
+        }
+
+        /// <summary>
+        /// File used to load/save the key/event mappings from.
+        /// </summary>
+        /// <param name="mapperFile">File used to load/save the key/event mappings from.</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddMapperFile(string mapperFile = null)
+        {
+            MapperFile = mapperFile;
+            return this;
+        }
+
+        /// <summary>
+        /// Avoid usage of symkeys, might not work on all operating systems
+        /// </summary>
+        /// <param name="useScanCodes">Avoid usage of symkeys, might not work on all operating systems</param>
+        /// <returns>DosBox SDL configuration to add another configuration</returns>
+        public SdlOptions AddUseScanCodes(bool? useScanCodes = true)
+        {
+            UseScanCodes = useScanCodes;
+            return this;
+        }
+
+        #endregion "Fluent"
     }
 }
