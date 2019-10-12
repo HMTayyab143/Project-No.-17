a1fa0b26aaacacdb04efad7cb15488dd3e1c359a
Ricardo Coelho <tiorac@msn.com>
2019-10-12 18:20:04 -0300

    DosBoxOptions

---
+++ TioRAC.DosBox/Options/DosBoxMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TioRAC.DosBox.Options
+{
+    public enum DosBoxMachine
+    {
+        Hercules,
+        CGA,
+        Tandy,
+        PCjr,
+        EGA,
+        VGAOnly,
+        SVGA_S3,
+        SVGA_et3000,
+        SVGA_et4000,
+        SVGA_Paradise,
+        Vesa_nolfb,
+        Vesa_oldvbe
+    }
+}
---
+++ TioRAC.DosBox/Options/DosBoxOptions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TioRAC.DosBox.Options
+{
+    public class DosBoxOptions : BaseOptions
+    {
+        #region "Constructions"
+
+        public DosBoxOptions()
+            : base("dosbox")
+        {
+        }
+
+        #endregion "Constructions"
+
+        #region "Fields"
+
+        private string _language;
+        private DosBoxMachine _machine;
+        private string _captures;
+        private uint _memSize;
+
+        #endregion "Fields"
+
+        #region "Properties"
+
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
+        public string Language
+        {
+            get
+            {
+                return _language;
+            }
+            set
+            {
+                _language = value;
+                OnPropertyChanged("Language");
+            }
+        }
+
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
+        public DosBoxMachine Machine
+        {
+            get
+            {
+                return _machine;
+            }
+            set
+            {
+                _machine = value;
+                OnPropertyChanged("Machine");
+            }
+        }
+
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
+        public string Captures
+        {
+            get
+            {
+                return _captures;
+            }
+            set
+            {
+                _captures = value;
+                OnPropertyChanged("Captures");
+            }
+        }
+
+        [DosBoxVersion(DosBoxVersion.Official0_74)]
+        public uint MemSize
+        {
+            get
+            {
+                return _memSize;
+            }
+            set
+            {
+                _memSize = value;
+                OnPropertyChanged("MemSize");
+            }
+        }
+
+        #endregion "Properties"
+
+
+
+
+
+        public override void LoadDictionary(IDictionary<string, object> dictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
