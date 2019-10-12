59f0bb039ef0ccf0969317b45644a018856e552f
Ricardo Coelho <tiorac@msn.com>
2019-10-12 12:21:11 -0300

    XML document for  PriorityLevel and VideoOutput

--- TioRAC.DosBox/Options/PriorityLevel.cs
+++ TioRAC.DosBox/Options/PriorityLevel.cs
@@ -4,13 +4,39 @@ using System.Text;
 
 namespace TioRAC.DosBox.Options
 {
+    /// <summary>
+    ///  Priority level for DosBox in <a href="https://www.dosbox.com/wiki/Dosbox.conf#.5Bsdl.5D">SDL</a> options
+    /// </summary>
     public enum PriorityLevel
     {
+        /// <summary>
+        /// Pause DosBox, only when minimized
+        /// </summary>
         Pause = 0,
-        Lowest, 
-        Lower, 
-        Normal, 
-        Higher, 
+
+        /// <summary>
+        /// Lowest priority
+        /// </summary>
+        Lowest,
+
+        /// <summary>
+        /// Lower priority
+        /// </summary>
+        Lower,
+
+        /// <summary>
+        /// Normal priority
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Higher priority
+        /// </summary>
+        Higher,
+
+        /// <summary>
+        /// Highest priority
+        /// </summary>
         Highest, 
     }
 }
--- TioRAC.DosBox/Options/VideoOutput.cs
+++ TioRAC.DosBox/Options/VideoOutput.cs
@@ -4,12 +4,34 @@ using System.Text;
 
 namespace TioRAC.DosBox.Options
 {
+    /// <summary>
+    /// Output options in <a href="https://www.dosbox.com/wiki/Dosbox.conf#.5Bsdl.5D">SDL</a> options
+    /// </summary>
     public enum VideoOutput
     {
+        /// <summary>
+        /// Output surface
+        /// </summary>
         Surface,
+
+        /// <summary>
+        /// Output overlay
+        /// </summary>
         Overlay,
+
+        /// <summary>
+        /// Output OpenGL
+        /// </summary>
         OpenGL,
+
+        /// <summary>
+        /// Output OpenGLln
+        /// </summary>
         OpenGLnb,
+
+        /// <summary>
+        /// Output DirectDraw
+        /// </summary>
         DDraw
     }
 }
